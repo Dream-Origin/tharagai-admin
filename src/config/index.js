74f87417c42da154d26ad660a630e2ae357c5bcf
rajeshmine <rajeshjas20296@gmail.com>
2026-06-15 12:08:11 +0530

    config change

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+    API_BASE_URL: "https://1sygpybr59.execute-api.ap-south-1.amazonaws.com",
+    PRODUCTS_API: "/products",
+    ORDERS_API: "/orders",  
+    // API_BASE_URL: "http://localhost:3001",
+};
